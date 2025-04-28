8f32209e008f64cccdab8608c6e21b427021d484
Julio Pires <julio.pires@ifgoiano.edu.br>
2025-04-28 10:15:12 -0300

    datepicker

--- android-aplicado/UsandoComponentesVisuais/app/src/main/java/br/edu/utfpr/usandocomponentesvisuais/MainActivity.kt
+++ android-aplicado/UsandoComponentesVisuais/app/src/main/java/br/edu/utfpr/usandocomponentesvisuais/MainActivity.kt
@@ -2,8 +2,8 @@ package br.edu.utfpr.usandocomponentesvisuais
 
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
 import android.widget.LinearLayout
-import android.widget.RatingBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,8 +12,7 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var window: LinearLayout
-    private lateinit var ratingBar: RatingBar
-
+    private lateinit var datePicker: DatePicker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +25,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         window = findViewById(R.id.main)
-        ratingBar = findViewById(R.id.ratingBar)
+        datePicker = findViewById(R.id.datePicker)
     }
 
     fun onTestComponentClick(view: View) {
-        Snackbar.make(window, "Avaliação: ${ratingBar.rating}", Snackbar.LENGTH_LONG).show()
+        val formattededDate = "${datePicker.dayOfMonth}/${datePicker.month + 1}/${datePicker.year}"
+        Snackbar.make(window, "Data: $formattededDate", Snackbar.LENGTH_LONG).show()
     }
 }
