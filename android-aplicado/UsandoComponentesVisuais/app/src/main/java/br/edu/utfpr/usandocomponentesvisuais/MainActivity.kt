c7bd93940522ad939f60ad04ee064f1b88a0948f
Julio Pires <julio.pires@ifgoiano.edu.br>
2025-04-28 10:41:05 -0300

    edittext

--- android-aplicado/UsandoComponentesVisuais/app/src/main/java/br/edu/utfpr/usandocomponentesvisuais/MainActivity.kt
+++ android-aplicado/UsandoComponentesVisuais/app/src/main/java/br/edu/utfpr/usandocomponentesvisuais/MainActivity.kt
@@ -4,7 +4,7 @@ import android.app.DatePickerDialog
 import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.View
-import android.widget.DatePicker
+import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,7 +14,7 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var window: LinearLayout
-    private lateinit var datePicker: DatePicker
+    private lateinit var etText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +27,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         window = findViewById(R.id.main)
+        etText = findViewById(R.id.etText)
+
+        etText.setOnClickListener {
+            onTestComponentClick()
+        }
     }
 
-    fun onTestComponentClick(view: View) {
+    fun onTestComponentClick() {
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
@@ -39,7 +44,7 @@ class MainActivity : AppCompatActivity() {
             this,
             { _, selectedYear, selectedMonth, selectedDay ->
                 val formattedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
-                Snackbar.make(window, "Data: $formattedDate", Snackbar.LENGTH_LONG).show()
+                etText.setText(formattedDate)
             },
             year,
             month,
