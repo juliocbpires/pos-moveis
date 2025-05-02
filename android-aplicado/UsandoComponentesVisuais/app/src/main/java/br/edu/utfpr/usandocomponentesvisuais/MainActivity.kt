b44efced49e7e9a1a69ceeac63def494f36a42ab
Julio Pires <julio.pires@ifgoiano.edu.br>
2025-05-02 15:25:25 -0300

    autocomplete textview

--- android-aplicado/UsandoComponentesVisuais/app/src/main/java/br/edu/utfpr/usandocomponentesvisuais/MainActivity.kt
+++ android-aplicado/UsandoComponentesVisuais/app/src/main/java/br/edu/utfpr/usandocomponentesvisuais/MainActivity.kt
@@ -1,20 +1,19 @@
 package br.edu.utfpr.usandocomponentesvisuais
 
-import android.app.DatePickerDialog
-import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
+import android.widget.AutoCompleteTextView
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var window: LinearLayout
-    private lateinit var etText: EditText
+    private lateinit var editText: AutoCompleteTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,30 +26,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         window = findViewById(R.id.main)
-        etText = findViewById(R.id.etText)
+        editText = findViewById(R.id.editText)
 
-        etText.setOnClickListener {
-            onTestComponentClick()
-        }
-    }
-
-    fun onTestComponentClick() {
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-        val dialog = DatePickerDialog(
-            this,
-            { _, selectedYear, selectedMonth, selectedDay ->
-                val formattedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
-                etText.setText(formattedDate)
-            },
-            year,
-            month,
-            day
+        val cities = listOf(
+            "Goiânia",
+            "Passa Quatro",
+            "Brasília",
+            "Curitiba",
+            "Pato Branco"
         )
 
-        dialog.show()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, cities)
+        editText.setAdapter(adapter)
+
+        editText.threshold = 2
+    }
+
+    fun onClickButton(view: View) {
+        Snackbar.make(window, editText.text, Snackbar.LENGTH_LONG).show()
     }
 }
